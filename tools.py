14b12df8c46d91281e170ceb8c7c0e31892cf84e
EddyTheB <mail@eddybarratt.co.uk>
2017-02-26 20:27:08 +0000

    Added camera functions

--- tools.py
+++ tools.py
@@ -2,6 +2,8 @@ import os
 import json
 import time
 import RPi.GPIO as GPIO
+from picamera import PiCamera
+from fractions import Fraction
 homeDir = os.path.expanduser('~')
 
 def getKey(key, file='Default'):
@@ -20,10 +22,42 @@ def onGPIO(pinNo, duration=60):
       time.sleep(duration)
       GPIO.output(pinNo, False)
   except KeyboardInterrupt:
-      GPIO.output(pinNo, False)      
+      GPIO.output(pinNo, False)
   GPIO.cleanup()
 
 def offGPIO(pinNo):
   GPIO.output(pinNo, False)
   GPIO.cleanup()
 
+def initCamera(mode='default'):
+  if mode == 'lowlight':
+    cam = PiCamera(resolution=(1280, 720), framerate=Fraction(1,6))
+    cam.shutter_speed = 6000000 # 6 seconds is the maximum
+    cam.iso = 800
+    # Give the camera a good long time to set gains and
+    # measure AWB (you may wish to use fixed AWB instead)
+    time.sleep(30)
+    cam.exposure_mode = 'off'
+  else:
+    cam = PiCamera()
+    cam.start_preview()
+    time.sleep(2)
+  return cam
+
+def closeCamera(cam):
+  cam.stop_preview()
+
+def takePhoto(cam, fileName='image.jpg'):
+  cam.capture(fileName)
+  return fileName
+
+def takeLowLightPhoto():
+  # turn on the lights.
+  onGPIO(7, duration=40)
+  # inititialize the camera.
+  cam = initCamera(mode='lowlight')
+  takePhoto(cam)
+
+
+if __name__ == '__main__':
+  takeLowLightPhoto()
