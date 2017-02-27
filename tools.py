95140d2f240c6d2b7385f0b6a3c559a6385e8fb6
EddyTheB <mail@eddybarratt.co.uk>
2017-02-27 20:10:42 +0000

    -

--- tools.py
+++ tools.py
@@ -26,17 +26,20 @@ def onGPIO(pinNo, duration=60):
   GPIO.cleanup()
 
 def offGPIO(pinNo):
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(pinNo, GPIO.OUT)
   GPIO.output(pinNo, False)
   GPIO.cleanup()
 
 def initCamera(mode='default'):
   if mode == 'lowlight':
     cam = PiCamera(resolution=(1280, 720), framerate=Fraction(1,6))
+    cam.color_effects = (128,128) # turn camera to black and white
     cam.shutter_speed = 6000000 # 6 seconds is the maximum
     cam.iso = 800
     # Give the camera a good long time to set gains and
     # measure AWB (you may wish to use fixed AWB instead)
-    time.sleep(30)
+    time.sleep(30) #(30)
     cam.exposure_mode = 'off'
   else:
     cam = PiCamera()
@@ -53,11 +56,16 @@ def takePhoto(cam, fileName='image.jpg'):
 
 def takeLowLightPhoto():
   # turn on the lights.
-  onGPIO(7, duration=40)
-  # inititialize the camera.
-  cam = initCamera(mode='lowlight')
-  takePhoto(cam)
-
+  onGPIO(7, duration=-1)
+  try:
+    # inititialize the camera.
+    cam = initCamera(mode='lowlight')
+    # take the photo
+    takePhoto(cam)
+    # turn off the lights.
+    offGPIO(7)
+  except KeyboardInterrupt:
+    offGPIO(7)
 
 if __name__ == '__main__':
   takeLowLightPhoto()
