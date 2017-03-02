2c38687da7d95ced3e8367850ebce62863b8fab2
EddyTheB <mail@eddybarratt.co.uk>
2017-03-02 21:14:18 +0000

    improving photo view

--- webCam/views.py
+++ webCam/views.py
@@ -23,6 +23,8 @@ def imageViewer(request, camID):
     if form.is_valid():
       print 'a'
       print form.cleaned_data
+      iso = form.cleaned_data.iso
+      ss = form.cleaned_data.shutterspeed
 
       # get the camera
       camera = get_object_or_404(Camera, pk=camID)
@@ -31,15 +33,30 @@ def imageViewer(request, camID):
       computer = get_object_or_404(Computer, pk=camera.computer_id)
 
       # Get the current date time
-      dt = datetime.now().strptime('%y%m%d_%H:%M:%S')
+      dt = datetime.now().strftime('%y%m%d_%H:%M:%S')
       print dt
 
       # create a filename to save to.
       fname = "Cam{}_{}.jpg".format(camID, dt)
 
-      # create a filename to save to.
-      fname = "image.jpg"
+      # connect to the computer
+      ssh = paramiko.SSHClient()
+      ssh.set_missing_host_key_policy(
+        paramiko.AutoAddPolicy())   # Shouldn't be neccesary, don't know why it
+                                    # doesn't recognise host.
+      ssh.connect(computer.ip_address, username=computer.user_name, password=getKey(computer.user_name))
 
+      command = "cd Documents/Development/; python tools.py takePhoto --iso {} --ss {} --fn {}".format(iso, ss, fname)
+      print command
+      stdin, stdout, stderr = ssh.exec_command(command)
+      # Some sort of error handling would be good...
+      #  print stderr.read()
+
+      # Now copy the file locally.
+      sftp = ssh.open_sftp()
+      sftp.get("Documents/Development/{}".format(fname), "webCam/static/webCam/images/blah.jpg")
+      sftp.close()
+      ssh.close()
 
       pass
 
@@ -50,14 +67,6 @@ def imageViewer(request, camID):
 
 
   """
-  # get the camera
-  camera = get_object_or_404(Camera, pk=camID)
-
-  # get the computer
-  computer = get_object_or_404(Computer, pk=camera.computer_id)
-
-  # create a filename to save to.
-  fname = "image.jpg"
 
   # connect to the computer
   ssh = paramiko.SSHClient()
