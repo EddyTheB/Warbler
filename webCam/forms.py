eaa2cdeed5af84f4f2bb0ab8c5958e34b2657203
EddyTheB <mail@eddybarratt.co.uk>
2017-03-02 19:37:29 +0000

    improving photo view

--- webCam/forms.py
+++ webCam/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class photoForm(forms.Form):
-  iso = forms.IntegerField(label='ISO', initial=800)
+  iso = forms.ChoiceField(label='ISO', choices=[100, 200, 300, 400, 500, 600, 700, 800], initial=800)
   shutterspeed = forms.FloatField(label='Shutter Speed', initial=6.0)
