87183863a9d3b105e81fa3fdb5e722c8a2fe2b42
EddyTheB <mail@eddybarratt.co.uk>
2017-03-03 21:09:35 +0000

    improving photo view

--- webCam/views.py
+++ webCam/views.py
@@ -55,6 +55,7 @@ def imageViewer(request, camID):
       ssh.connect(computer.ip_address, username=computer.user_name, password=getKey(computer.user_name))
 
       command = "cd Documents/Development/; python tools.py takePhoto --iso {} --ss {} --tt {} --fn {} --resw 720 --resh 1280".format(iso, ss, dt_text, fname)
+      print command
       stdin, stdout, stderr = ssh.exec_command(command)
       # Some sort of error handling would be good...
       #  print stderr.read()
